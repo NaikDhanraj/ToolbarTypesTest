0e1d8aa4e01f538309293a41b04f28650c9e609f
NaikDhanraj <dssp522@gmail.com>
2016-07-18 14:58:44 +0530

    added toolbar collapse with single item and not recyclerview

---
+++ app/src/main/java/com/dssp/toolbartypestest/ToolbarCollapse.java
@@ -0,0 +1,15 @@
+package com.dssp.toolbartypestest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dhanrajnaik522 on 7/18/2016.
+ */
+public class ToolbarCollapse extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toolbar_collapse);
+    }
+}
